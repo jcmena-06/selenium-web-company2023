9e42c8318d9e18d5a3509e5c6cadf34714163dfc
jcmena <jcmena@localhost>
2023-01-22 21:11:15 -0600

    Two new classes Suma and Concatenacion

---
+++ SeleniumCoolTesters/src/test/java/com/google/qa/Concatenacion.java
@@ -0,0 +1,14 @@
+package com.google.qa;
+
+public class Concatenacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 	String str1 = "Hola";
+	        String str2 = "mundo";
+	        String str3 = "!";
+	        String frase = str1 + " " + str2 + str3;
+	        System.out.println("La frase formada es: " + frase);
+	}
+
+}
---
+++ SeleniumCoolTesters/src/test/java/com/google/qa/Suma.java
@@ -0,0 +1,15 @@
+package com.google.qa;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+	    int num2 = 20;
+	    int num3 = 30;
+	    int suma = num1 + num2 + num3;
+	    System.out.println("La suma de los números es: " + suma);
+
+	}
+
+}
